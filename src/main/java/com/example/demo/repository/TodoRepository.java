8ad87da018369e966fb818677f02a1da5b6ecb51
Reza Khaleghi <reza.khaleghi@lhasalimited.org>
2018-01-16 17:19:59 +0000

    added repo

---
+++ src/main/java/com/example/demo/repository/TodoRepository.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.example.demo.repository;
+
+
+/**
+ * @author RezaKhaleghi
+ *
+ */
+public interface TodoRepository extends JpaRepository<Todo, Long>
+{
+
+}
+
+/* ---------------------------------------------------------------------*
+ * This software is the confidential and proprietary
+ * information of Lhasa Limited
+ * Granary Wharf House, 2 Canal Wharf, Leeds, LS11 5PS
+ * ---
+ * No part of this confidential information shall be disclosed
+ * and it shall be used only in accordance with the terms of a
+ * written license agreement entered into by holder of the information
+ * with LHASA Ltd.
+ * --------------------------------------------------------------------- */
